2c933054a9738224a9a0566f30b297fc1d2c1057
yudedako <pluto.club@gmail.com>
2019-04-07 15:16:56 +0900

    ログイン後のページを作成

---
+++ components/members-only.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="loaded">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex'
+
+export default {
+  data() {
+    return {
+      loaded: false
+    }
+  },
+
+  computed: {
+    ...mapGetters(['isAuthenticated'])
+  },
+
+  async mounted() {
+    setTimeout(() => {
+      if (!this.isAuthenticated) {
+        this.$router.push('account')
+      }
+      this.loaded = true
+    }, 0)
+  }
+}
+</script>
---
+++ pages/member-page.vue
@@ -0,0 +1,16 @@
+<template>
+  <members-only>
+    <h1>ログインしていないと見れないページ</h1>
+    <a href="/account">アカウントページへ</a>
+  </members-only>
+</template>
+
+<script>
+import membersOnly from '~/components/members-only.vue'
+
+export default {
+  components: {
+    membersOnly
+  }
+}
+</script>
